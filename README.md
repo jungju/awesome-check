1f857a0263f7a4cffd8764d1025e3534e4a22221
jungju <leejungju.go@gmail.com>
2019-04-02 01:17:10 +0900

    update all

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Awesome check
 
-## Serverless - Framework
+
+## Serverless-Frameworks
 
 |Name|Stargazers Count|License Name|
 |---|---|---|
 |OpenFaaS|13709|MIT License|
-|Zappa|8859|MIT License|
+|Zappa|8860|MIT License|
 |Apex|7639|MIT License|
 |Up|6809|Other|
 |Chalice|5475|Apache License 2.0|
@@ -18,15 +19,15 @@
 |Kappa|897|Apache License 2.0|
 |Jets|792|MIT License|
 |Riff|685|Other|
-|middy|575|MIT License|
 |Lambdoku|575||
+|middy|575|MIT License|
 |Sparta|548|MIT License|
 |DEEP|528|Mozilla Public License 2.0|
 |Spring Cloud Function|483|Apache License 2.0|
 |Shep|355|MIT License|
 |Lambda API|290|MIT License|
-|Lambada Framework|236|MIT License|
 |Aegis|236|Apache License 2.0|
+|Lambada Framework|236|MIT License|
 |Fission Workflows|218|Apache License 2.0|
 |Turtle|151|Apache License 2.0|
 |Architect|142||
@@ -40,9 +41,34 @@
 |Lambda Forest|10|MIT License|
 |flowbject|6||
 |DropFaaS|4|GNU Lesser General Public License v3.0|
-|Serverless Framework|0||
-|Gestalt Framework|0||
-|Sigma|0||
 |AWS Amplify|0||
 |FuseLess|0||
+|Gestalt Framework|0||
 |FunctionShield|0||
+|Sigma|0||
+|Serverless Framework|0||
+
+
+## Kubernetes-Configuration
+
+|Name|Stargazers Count|License Name|
+|---|---|---|
+|Kompose|3736|Apache License 2.0|
+|Jsonnet|2731|Apache License 2.0|
+|kustomize|2319|Apache License 2.0|
+|Habitat|1883|Apache License 2.0|
+|kapitan|731|Apache License 2.0|
+|Konfd|523|Other|
+|kubediff|523|Other|
+|kubecfg|416|Apache License 2.0|
+|kenv|141|MIT License|
+|kubegen|116|Other|
+|Pulumi|61|Apache License 2.0|
+|Ktmpl|60|MIT License|
+|thesus|27|Apache License 2.0|
+|K8comp|21|Apache License 2.0|
+|Ansible|0||
+|Puppet|0||
+|Chef|0||
+|Saltstack|0||
+
--- main.go
+++ main.go
@@ -11,6 +11,14 @@ import (
 	"golang.org/x/oauth2"
 )
 
+// Category category
+type Category struct {
+	Title      string              `yaml:"title"`
+	URL        string              `yaml:"url"`
+	MapService map[string]*Service `yaml:"services"`
+	Services   []*Service          `yaml:"-"`
+}
+
 // Service service
 type Service struct {
 	Name            string `yaml:"name"`
@@ -21,8 +29,14 @@ type Service struct {
 	LicenseName     string
 }
 
-func main() {
-	token := os.Getenv("GITHUB_TOKEN")
+// Checker checker
+type Checker struct {
+	Client     *github.Client
+	Categories []*Category
+}
+
+// NewChecker newChecker
+func NewChecker(token string) *Checker {
 	if token == "" {
 		log.Fatal("Requier token")
 	}
@@ -31,41 +45,64 @@ func main() {
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 	tc := oauth2.NewClient(ctx, ts)
 
-	mapService := map[string]*Service{}
-	mustYamlFileToGoObject("serverless-framework.yml", mapService)
+	return &Checker{
+		Client: github.NewClient(tc),
+	}
+}
+func main() {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		log.Fatal("Requier token")
+	}
 
-	client := github.NewClient(tc)
+	checker := NewChecker(token)
+	checker.addCategoies("serverless-framework.yml", "kubernetes-configuration.yml")
 
-	services := []*Service{}
-	for k, v := range mapService {
+	readmeTmpl := mustRead("README.md.tmpl")
+	output, err := executeTemplateSource(string(readmeTmpl), checker.Categories, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mustWrite("README.md", output)
+}
+
+func (c *Checker) addCategoies(categoryPaths ...string) {
+	for _, categoryPath := range categoryPaths {
+		category := c.createCategory(categoryPath)
+		c.Categories = append(c.Categories, category)
+	}
+}
+
+//func (c *Checker) serviceGroup(metaPath, templatePath string) string {
+func (c *Checker) createCategory(metaPath string) *Category {
+	category := &Category{}
+	mustYamlFileToGoObject(metaPath, category)
+
+	category.Services = []*Service{}
+	for k, v := range category.MapService {
 		v.Name = k
 		if v.GithubRepo != "" {
 			userAndRepo := strings.Split(v.GithubRepo, "/")
-			rep, _, err := client.Repositories.Get(context.Background(), userAndRepo[0], userAndRepo[1])
-			if err != nil {
+			if rep, _, err := c.Client.Repositories.Get(context.Background(), userAndRepo[0], userAndRepo[1]); err == nil {
+				if rep.StargazersCount != nil {
+					v.StargazersCount = *rep.StargazersCount
+				}
+				if rep.License != nil {
+					v.LicenseName = rep.License.GetName()
+				}
+			} else {
 				log.Fatal(err)
 			}
-			if rep.StargazersCount != nil {
-				v.StargazersCount = *rep.StargazersCount
-			}
-			if rep.License != nil {
-				v.LicenseName = rep.License.GetName()
-			}
 		}
-		services = append(services, v)
+		category.Services = append(category.Services, v)
 	}
-
-	sortService(services)
-
-	mustWrite("README.md", mustExecuteReadmemd(services))
+	sortService(category.Services)
+	return category
 }
 
 func sortService(services []*Service) {
 	sort.Slice(services, func(i, j int) bool {
 		return services[i].StargazersCount > services[j].StargazersCount
 	})
-}
-
-func mustExecuteReadmemd(services []*Service) string {
-	return mustExecuteTemplateFile("./README.md.tmpl", services, nil)
 }
